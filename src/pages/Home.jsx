90dee1a5a47d806a98b5cfc0820b633ec08386bf
Farrel Najib Anshary <farrelnajib@gmail.com>
2020-12-18 22:49:00 +0700

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css'
+import Cards from '../components/Cards/Cards'
+import Hero from '../components/Hero/Hero'
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Cards />
+    </>
+  )
+}
+
+export default Home
